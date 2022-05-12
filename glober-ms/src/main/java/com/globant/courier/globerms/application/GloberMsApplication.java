fca934c054c4fcd167d3989bda370f8af14a792d
Joseph <joseph.rodriguez@globant.com>
2022-05-12 13:20:32 -0500

    New commit

---
+++ glober-ms/src/main/java/com/globant/courier/globerms/application/GloberMsApplication.java
@@ -0,0 +1,13 @@
+package com.globant.courier.globerms.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloberMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloberMsApplication.class, args);
+	}
+
+}
---
+++ glober-ms/src/test/java/com/globant/courier/globerms/GloberMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.courier.globerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloberMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
