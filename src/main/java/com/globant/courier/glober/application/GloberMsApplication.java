4cd7c7663327feb8813a6e20dc6e42eb83caec23
Joseph <joseph.rodriguez@globant.com>
2022-05-13 08:51:21 -0500

    empty folders

--- src/main/java/com/globant/courier/glober/application/GloberMsApplication.java
+++ src/main/java/com/globant/courier/glober/application/GloberMsApplication.java
@@ -1,4 +1,4 @@
-package com.globant.courier.globerms.application;
+package com.globant.courier.glober.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/globant/courier/glober/GloberMsApplicationTests.java
+++ src/test/java/com/globant/courier/glober/GloberMsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.globant.courier.globerms;
+package com.globant.courier.glober;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
